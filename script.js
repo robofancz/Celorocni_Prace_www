75e32a2c404a980660399702cada57665eb33e90
robofancz <robofancz@gmail.com>
2025-04-28 14:23:44 +0200

    light

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function light() {
     var element = document.body;
     element.classList.toggle("lightmode");
   }
+
+
+  const toggleButton = document.getElementById('light');
+
+  // Load preference on page load
+  if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('lightmode');
+  }
+
+  toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('lightmode');
+    
+    // Save the preference
+    if (document.body.classList.contains('lightmode')) {
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      localStorage.setItem('darkMode', 'disabled');
+    }
+  });
